18ea3e7b54b616749a76adad7e551e4ccc6d83d1
Anup Dhamala <anupdhml+git@gmail.com>
2023-06-15 15:57:45 +0200

    Rename whitney roots file to remove class info and refactor root parsing to support fifth class
    Signed-off-by: Anup Dhamala <anupdhml+git@gmail.com>

--- lib/roots.py
+++ lib/roots.py
@@ -1,13 +1,73 @@
-
-CLASS_HEADER = "6. nā-class"
 EARLIER_LANGUAGE_HEADER = "A. Earlier Language"
 EARLIER_AND_LATER_LANGUAGE_HEADER = "B. Earlier and Later Language"
 LATER_LANGUAGE_HEADER = "C. Later Language"
 
+CLASS_HEADER = "-class" # matches both "4. nu­class" and "6. nā-class"
+
+NINTH_CLASS = "ninth"
 NINTH_CLASS_STRONG_MARKER = "ā"
 NINTH_CLASS_WEAK_MARKER = "ī"
 
-def parse_whitney_rootline(line):
+FIFTH_CLASS = "fifth"
+FIFTH_CLASS_STRONG_MARKER = "o"
+FIFTH_CLASS_WEAK_MARKER = "u"
+
+
+def adjust_root_guess_to_vedaweb(root, variant_no, present_class):
+    if present_class == NINTH_CLASS:
+        # FIXME better place to keep these?
+        if root == "pu":
+            root = "pū"
+        elif root == "ju":
+            root = "jū"
+        elif root == "ji":
+            root = "jī"
+        elif root == "vr̥" and variant_no == '2':
+            # variant_no 1 'cover' is attested only in AV. 2 is 'choose'
+            # FIXME set stem as variant here too and ensure both are searched for
+            # but long variant doesn't have any hits here so safe to ignore this here
+            #root = "vr̥ ~ vr̥̄"
+            # also if we pass it like this, we should also change the stems here
+            root = "vr̥ vr̥̄" # other root is vr̥(1, cover which won't match)
+        # FIXME better way to pass these exceptions
+        # modifying the root here ensures for now we only find matches for the variants
+        # used in vedaweb
+        # SrI 1 mix: yes
+        # SrI 2 boil: not (VB: SrInati..)
+        elif root == "śrī" and variant_no == '2': # boil
+            root = "śrī 2" # other root is śrī (1, mix) which WILL match
+        # aS 1 attain: not (only in mahabhharata 'aSnIs')
+        # aS 2 eat yes
+        elif root == "aś" and variant_no == '1': # eat
+            root = "aśⁱ" # other root is aś (2, attain) which won't match
+        # gR as gr̥̄ 1 "sing" attested
+        # gR 2 "swallow" not (only in AVS)
+        elif root == "gr̥" and variant_no == '1': # sing
+            root = "gr̥̄ 1" # other root is gr̥(2, swallow) which won't match
+        #elif variant_no:
+            # FIXME assign vedaweb equivalent variant numbers here?
+        #    root += ' ' + variant_no
+
+    elif present_class == FIFTH_CLASS:
+        pass
+
+    else:
+        raise Exception(f"Unsupported present class: {present_class}")
+
+
+    return root
+
+
+def parse_whitney_rootline(line, present_class):
+    if present_class == NINTH_CLASS:
+        class_strong_marker = NINTH_CLASS_STRONG_MARKER
+        class_weak_marker = NINTH_CLASS_WEAK_MARKER
+    elif present_class == FIFTH_CLASS:
+        class_strong_marker = FIFTH_CLASS_STRONG_MARKER
+        class_weak_marker = FIFTH_CLASS_WEAK_MARKER
+    else:
+        raise Exception(f"Unsupported present class: {present_class}")
+
     variant_no = None
     attestation_texts = None
     weak_only = False
@@ -20,7 +80,7 @@ def parse_whitney_rootline(line):
         attestation_texts = " ".join(line_parts)
 
     # this is an assumption safe to make for our data even if there's multiple stem variants
-    if stem.endswith(NINTH_CLASS_WEAK_MARKER):
+    if present_class == NINTH_CLASS and stem.endswith(class_weak_marker):
         weak_only = True
 
     # if stem has a variant it will be marked '/'
@@ -36,12 +96,12 @@ def parse_whitney_rootline(line):
         # in the grammars/ dictionaries
         root_variants.append(stem_variant[:-2])
 
-        if stem_variant.endswith(NINTH_CLASS_WEAK_MARKER):
+        if stem_variant.endswith(class_weak_marker):
             weak_stem_variants.append(stem_variant)
-            strong_stem_variants.append(stem_variant[:-1] + NINTH_CLASS_STRONG_MARKER)
+            strong_stem_variants.append(stem_variant[:-1] + class_strong_marker)
         else:
             strong_stem_variants.append(stem_variant)
-            weak_stem_variants.append(stem_variant[:-1] + NINTH_CLASS_WEAK_MARKER)
+            weak_stem_variants.append(stem_variant[:-1] + class_weak_marker)
     root_variants = sorted(list(set(root_variants)))
 
     # set the final strings
@@ -49,38 +109,7 @@ def parse_whitney_rootline(line):
     strong_stem = ' '.join(strong_stem_variants)
     weak_stem = ' '.join(weak_stem_variants)
 
-    # FIXME better place to keep these?
-    if root == "pu":
-        root = "pū"
-    elif root == "ju":
-        root = "jū"
-    elif root == "ji":
-        root = "jī"
-    elif root == "vr̥" and variant_no == '2':
-        # variant_no 1 'cover' is attested only in AV. 2 is 'choose'
-        # FIXME set stem as variant here too and ensure both are searched for
-        # but long variant doesn't have any hits here so safe to ignore this here
-        #root = "vr̥ ~ vr̥̄"
-        # also if we pass it like this, we should also change the stems here
-        root = "vr̥ vr̥̄" # other root is vr̥(1, cover which won't match)
-    # FIXME better way to pass these exceptions
-    # modifying the root here ensures for now we only find matches for the variants
-    # used in vedaweb
-    # SrI 1 mix: yes
-    # SrI 2 boil: not (VB: SrInati..)
-    elif root == "śrī" and variant_no == '2': # boil
-        root = "śrī 2" # other root is śrī (1, mix) which WILL match
-    # aS 1 attain: not (only in mahabhharata 'aSnIs')
-    # aS 2 eat yes
-    elif root == "aś" and variant_no == '1': # eat
-        root = "aśⁱ" # other root is aś (2, attain) which won't match
-    # gR as gr̥̄ 1 "sing" attested
-    # gR 2 "swallow" not (only in AVS)
-    elif root == "gr̥" and variant_no == '1': # sing
-        root = "gr̥̄ 1" # other root is gr̥(2, swallow) which won't match
-    #elif variant_no:
-        # FIXME assign vedaweb equivalent variant numbers here?
-    #    root += ' ' + variant_no
+    root = adjust_root_guess_to_vedaweb(root, variant_no, present_class)
 
     return {
         "root_guess": root, # this is really an estimated guess based on the stem
@@ -91,30 +120,41 @@ def parse_whitney_rootline(line):
         "attestation_texts": attestation_texts,
     }
 
+###############################################################################
 
-def parse_whitney_roots(filepath):
+def parse_whitney_roots(filepaths):
     whitney_roots = []
 
-    with open(filepath, 'r') as whitney_file:
-        language_period = None
+    for filepath in filepaths:
+        present_class = None
+        if "ninth_class" in filepath:
+            present_class = NINTH_CLASS
+        elif "fifth_class" in filepath:
+            present_class = FIFTH_CLASS
 
-        while line := whitney_file.readline():
-            line = line.rstrip()
+        with open(filepath, 'r') as whitney_file:
+            language_period = None
 
-            if not line or CLASS_HEADER in line:
-                continue
-            elif EARLIER_LANGUAGE_HEADER in line:
-                language_period = "Earlier"
-                continue
-            elif EARLIER_AND_LATER_LANGUAGE_HEADER in line:
-                language_period = "Earlier & Later"
-                continue
-            elif LATER_LANGUAGE_HEADER in line:
-                language_period = "Later"
-                continue
+            while line := whitney_file.readline():
+                line = line.rstrip()
 
-            whitney_roots.append(
-                parse_whitney_rootline(line) | {"language_period": language_period}
-            )
+                if not line or CLASS_HEADER in line:
+                    continue
+                elif EARLIER_LANGUAGE_HEADER in line:
+                    language_period = "Earlier"
+                    continue
+                elif EARLIER_AND_LATER_LANGUAGE_HEADER in line:
+                    language_period = "Earlier & Later"
+                    continue
+                elif LATER_LANGUAGE_HEADER in line:
+                    language_period = "Later"
+                    continue
+
+                whitney_roots.append(
+                    parse_whitney_rootline(line, present_class) | {
+                        "language_period": language_period,
+                        #"present_class": present_class,
+                    }
+                )
 
     return whitney_roots
